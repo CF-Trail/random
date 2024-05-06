70b7ce30b36c96a224a0d023f791694474015372
szze <106690765+CF-Trail@users.noreply.github.com>
2024-05-07 02:00:41 +0900

    Create clogevade.lua

---
+++ .x.vr.e.hi/misc/clogevade.lua
@@ -0,0 +1,34 @@
+[Evade]
+[2/18/2023]
+[~] fixed Fast Revive not healing sometimes
+
+[1/1/2023]
+[+] gift autofarm
+happy new year
+
+[11/28/2022]
+[+] added tools spammer
+[~] fixed autowin breaking
+[~] fixed dying with autowin :skull emoji:
+
+[12/27/2022]
+[-] Removed halloween toggles (lol)
+Merry christmas mate!
+
+[11/8/2022]
+[+] Teleport to downed
+
+[~] Made walkspeed boost smoother. I recommend sticking to 1 speed instead of 2
+
+[11/5/2022]
+[+] Fast Revive
+
+[11/4/2022]
+gotcha bugxie!
+[+] Added ESP
+[+] Added No Water Damage
+
+[10/27/2022]
+
+[+] Round End Timer
+
