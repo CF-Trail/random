cb22d723a4e3d80b32e3bbbba58874f03bb314fe
szze <106690765+CF-Trail@users.noreply.github.com>
2024-05-07 02:00:06 +0900

    Add files via upload

---
+++ .x.vr.e.hi/loader.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == 2597632885 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/.x.vr.e.hi/snoday.lua"))()
+  elseif game.PlaceId == 6839171747 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/.x.vr.e.hi/doors.lua"))()
+ elseif game.GameId == 3647333358 or game.PlaceId == 9872472334 then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/.x.vr.e.hi/evade.lua"))()
+  elseif game.GameId == 323675642 then
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/CF-Trail/random/main/.x.vr.e.hi/fe2.lua"))()
+end
