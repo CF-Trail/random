28d466911ae3764a8f4e701dbf6127c0fb9eac69
szze <106690765+CF-Trail@users.noreply.github.com>
2025-07-05 20:54:12 +0300

    Update pricessab.lua

--- pricessab.lua
+++ pricessab.lua
@@ -3,5 +3,6 @@ return {
     ['graipuss medussi'] = '$1.12',
     ['los tralaleritos'] = '$0.71',
     ['la grande combinasion'] = '$5.51',
-    ['sammyni spyderini'] = '$0.40'
+    ['sammyni spyderini'] = '$0.40',
+    ['garama and madundung'] = '$104'
 }
