c26fb634a12a96b25150c76b509a98b0c7b1d3af
szze <106690765+CF-Trail@users.noreply.github.com>
2025-07-06 17:10:12 +0300

    Update pricessab.lua

--- pricessab.lua
+++ pricessab.lua
@@ -1,8 +1,8 @@
 return {
     ['la vacca saturno saturnita'] = '$0.37',
-    ['graipuss medussi'] = '$0.71',
-    ['los tralaleritos'] = '$0.59',
-    ['la grande combinasion'] = '$4.45',
-    ['sammyni spyderini'] = '$0.21',
-    ['garama and madundung'] = '$29.68'
+    ['graipuss medussi'] = '$1.04',
+    ['los tralaleritos'] = '$0.67',
+    ['la grande combinasion'] = '$5.19',
+    ['sammyni spyderini'] = '$0.27',
+    ['garama and madundung'] = '$32.94'
 }
