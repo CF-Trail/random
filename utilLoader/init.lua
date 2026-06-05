ce7fbd1c57da62cbbf6dc9a2c64bbdf7fc30ab46
szze <106690765+CF-Trail@users.noreply.github.com>
2026-06-05 14:57:29 +0300

    Update init.lua

--- utilLoader/init.lua
+++ utilLoader/init.lua
@@ -1,295 +1,177 @@
 if not hookmetamethod or not hookfunction then
-	repeat
-		task.wait()
-	until game:GetService('Players').LocalPlayer
-	game:GetService('Players').LocalPlayer:Kick('Bad executor')
+    repeat task.wait() until game:GetService("Players").LocalPlayer
+    game:GetService("Players").LocalPlayer:Kick("Bad executor")
+    return
 end
 
 if not rconsoleprint then
-	rconsoleprint = function(...)
-		print(...)
-	end
+    rconsoleprint = function(...) print(...) end
 end
 
-local closure = syn_newcclosure or newcclosure or function (w)
-	--aaa
-end
+local closure = syn_newcclosure or newcclosure or function(f) return f end
 
 local ANSI = {
-    RESET         = "\27[0m",
-
-    BLACK         = "\27[30m",
-    RED           = "\27[31m",
-    GREEN         = "\27[32m",
-    YELLOW        = "\27[33m",
-    BLUE          = "\27[34m",
-    MAGENTA       = "\27[35m",
-    CYAN          = "\27[36m",
-    WHITE         = "\27[37m",
-
-    LIGHT_RED     = "\27[91m",
-    LIGHT_GREEN   = "\27[92m",
-    LIGHT_YELLOW  = "\27[93m",
-    LIGHT_BLUE    = "\27[94m",
+    RESET = "\27[0m",
+    BLUE  = "\27[34m",
+    GREEN = "\27[32m",
     LIGHT_MAGENTA = "\27[95m",
-    LIGHT_CYAN    = "\27[96m",
 }
 
+local function blueRPRINT(t)    rconsoleprint(ANSI.BLUE          .. t .. ANSI.RESET) end
+local function LMagentaPrint(t)  rconsoleprint(ANSI.LIGHT_MAGENTA .. t .. ANSI.RESET) end
+local function GreenPrint(t)     rconsoleprint(ANSI.GREEN         .. t .. ANSI.RESET) end
 
-local function blueRPRINT(text)
-    rconsoleprint(ANSI.BLUE .. text .. ANSI.RESET)
-end
+local Players          = game:GetService("Players")
+local CoreGui          = game:GetService("CoreGui")
+local ContentProvider  = game:GetService("ContentProvider")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-local function LMagentaPrint(text)
-    rconsoleprint(ANSI.LIGHT_MAGENTA .. text .. ANSI.RESET)
-end
-
-local function GreenPrint(text)
-    rconsoleprint(ANSI.GREEN .. text .. ANSI.RESET)
-end
-
---// VoiceChat spoof
-
-local oldhOwOok
-oldhOwOok = hookmetamethod(game, "__namecall", closure(function(self, ...)
-	if getnamecallmethod() == "IsVoiceEnabledForUserIdAsync" then
-		return true
-	end
-	return oldhOwOok(self, ...)
-end))
-
---// PreloadBypass
+repeat task.wait() until Players.LocalPlayer
+local lplr  = Players.LocalPlayer
+local isAFK = false
+local isAdonis = false
 
 local tbl = {}
-local CollectionService = game:GetService('CollectionService')
-local CoreGui = game:GetService('CoreGui')
-
-for i, v in pairs(CoreGui.GetDescendants(CoreGui)) do
-	if v.IsA(v, "ImageLabel") and v.Image:find('rbxasset://') then
-		table.insert(tbl, v.Image)
-	end
+for _, v in pairs(CoreGui:GetDescendants()) do
+    if v:IsA("ImageLabel") and v.Image:find("rbxasset://") then
+        table.insert(tbl, v.Image)
+    end
 end
 
-local hello;
-hello = hookfunction(game:GetService("ContentProvider").PreloadAsync, closure(function(self, ...)
-	local Args = {
-		...
-	}
-	if not checkcaller() and type(Args[1]) == "table" and table.find(Args[1], CoreGui) then
-		LMagentaPrint("szze's utilities | Blocked PreloadAsync detection" .. ' | Method used: __index \n')
-		Args[1] = tbl
-		return hello(self, unpack(Args))
-	end
-	return hello(self, ...)
-end))
-
-local function football(ncm)
-	if ncm == "PreloadAsync" or ncm == "preloadAsync" then
-		return true
-	end
-	return false
-end
-
-local __namecall;
-
-__namecall = hookmetamethod(game, "__namecall", closure(function(Self, ...)
-	local Args = {
-		...
-	}
-	local NamecallMethod = getnamecallmethod()
-	if not checkcaller() and type(Args[1]) == "table" and table.find(Args[1], CoreGui) and Self == game.GetService(game, "ContentProvider") and football(NamecallMethod) then
-		LMagentaPrint("szze's utilities | Blocked PreloadAsync detection from " .. getcallingscript():GetFullName() .. ' | Method used: __namecall \n')
-		Args[1] = tbl
-		return __namecall(Self, unpack(Args))
-	end
-	return __namecall(Self, ...)
-end))
-
---// Local GamePass bypasser
-
-local owold
-owold = hookmetamethod(game, '__namecall', closure(function(...)
-	local method = getnamecallmethod()
-	if method == 'UserOwnsGamePassAsync' and not checkcaller() then
-		return true
-	end
-	return owold(...)
-end))
-
--- // Memory bypass
-
-local uwu
-uwu = hookmetamethod(game, '__namecall', closure(function(...)
-	local method = getnamecallmethod()
-	if method == 'GetTotalMemoryUsageMb' then
-		return math.random(400, 450)
-	end
-	return uwu(...)
-end))
-
---// AntiShutdown
-
-hookfunction(game.Shutdown, closure(function(...)
-	local args = { ... }
-	if not checkcaller() then
-		LMagentaPrint("szze's utilities | Blocked kick " .. '| Method used: __index [SHUTDOWN] \n')
-		return wait(9e9)
-	end
-end))
-
-local twt
-twt = hookmetamethod(game, '__namecall', closure(function(self, ...)
-	local kscriptz, kscript
-	local method = getnamecallmethod()
-	if self == game and string.lower(method) == 'shutdown' and not checkcaller() then
-		kscriptz = getcallingscript()
-		if kscriptz then
-			kscript = kscript:GetFullName()
-		else
-			kscript = "Couldn't fetch"
-		end
-		LMagentaPrint("szze's utilities | Blocked kick from " .. tostring(kscript) .. ' | Method used: __namecall [SHUTDOWN] \n')
-		return wait(9e9)
-	end
-	return twt(self, ...)
-end))
-
---// Script
-
-repeat
-	task.wait()
-until game:GetService('Players').LocalPlayer
-local lplr = game:GetService('Players').LocalPlayer
-local isAFK = false
-
-blueRPRINT(
-    [[ 
-    _____   ______  ______  ______   _    _____     _    _   _______   _____   _        _____   _______   _____   ______    _____ 
+blueRPRINT([[
+    _____   ______  ______  ______   _    _____     _    _   _______   _____   _        _____   _______   _____   ______    _____
     / ____| |___  / |___  / |  ____| ( )  / ____|   | |  | | |__   __| |_   _| | |      |_   _| |__   __| |_   _| |  ____|  / ____|
-   | (___      / /     / /  | |__    |/  | (___     | |  | |    | |      | |   | |        | |      | |      | |   | |__    | (___  
-    \___ \    / /     / /   |  __|        \___ \    | |  | |    | |      | |   | |        | |      | |      | |   |  __|    \___ \ 
+   | (___      / /     / /  | |__    |/  | (___     | |  | |    | |      | |   | |        | |      | |      | |   | |__    | (___
+    \___ \    / /     / /   |  __|        \___ \    | |  | |    | |      | |   | |        | |      | |      | |   |  __|    \___ \
     ____) |  / /__   / /__  | |____       ____) |   | |__| |    | |     _| |_  | |____   _| |_     | |     _| |_  | |____   ____) |
-   |_____/  /_____| /_____| |______|     |_____/     \____/     |_|    |_____| |______| |_____|    |_|    |_____| |______| |_____/ 
-                                                                                                                                   
-                                                              Made by szze
-                                                                  V1.4
-                                                    
-    ]] .. '\n'
-)
+   |_____/  /_____| /_____| |______|     |_____/     \____/     |_|    |_____| |______| |_____|    |_|    |_____| |______| |_____/
 
---// AntiKick: __namecall
+                                                            Made by szze#6220
+                                                                  V1.5
+]] .. "\n")
 
-local OldNameCall 
-OldNameCall = hookmetamethod(game, "__namecall", closure(function(...)
-	local Self, Args = (...), ({
-		select(2, ...),
-	})
-	local kscript, kscriptz
-	if string.lower(getnamecallmethod()) == "kick" and Self == lplr and not checkcaller() then
-		kscriptz = getcallingscript()
-		if kscriptz then
-			kscript = kscriptz:GetFullName()
-		else
-			kscript = "Couldn't fetch"
-		end
-		LMagentaPrint("szze's utilities | Blocked kick from " .. tostring(kscript) .. ' | Method used: __namecall \n')
-		return wait(9e9)
-	end
-	return OldNameCall(...)
+local statsService = game:GetService("Stats")
+local totalMemory = statsService:GetTotalMemoryUsageMb()
+
+local oldNamecall
+oldNamecall = hookmetamethod(game, "__namecall", closure(function(self, ...)
+    local method = getnamecallmethod()
+
+    if method == "IsVoiceEnabledForUserIdAsync" then
+        return true
+    elseif method == "GetTotalMemoryUsageMb" then
+		return math.random(totalMemory - 25, totalMemory + 25)
+    end
+
+    if not checkcaller() then
+        local args = { ... }
+
+        if method == "UserOwnsGamePassAsync" or method == "PlayerHasPass" then
+            return true
+
+        elseif string.lower(method) == "kick" and self == lplr then
+            local s = getcallingscript()
+            LMagentaPrint("szze's utilities | Blocked kick from " .. (s and s:GetFullName() or "Couldn't fetch") .. " | Method used: __namecall \n")
+            return wait(9e9)
+
+        elseif string.lower(method) == "destroy" and self == lplr then
+            local s = getcallingscript()
+            LMagentaPrint("szze's utilities | Blocked kick from " .. (s and s:GetFullName() or "Couldn't fetch") .. " | Method used: __namecall [DESTROY] \n")
+            return wait(9e9)
+
+        elseif string.lower(method) == "shutdown" and self == game then
+            local s = getcallingscript()
+            LMagentaPrint("szze's utilities | Blocked kick from " .. (s and s:GetFullName() or "Couldn't fetch") .. " | Method used: __namecall [SHUTDOWN] \n")
+            return wait(9e9)
+
+        elseif (method == "PreloadAsync" or method == "preloadAsync")
+            and self == ContentProvider
+            and type(args[1]) == "table" and table.find(args[1], CoreGui) then
+            local s = getcallingscript()
+            LMagentaPrint("szze's utilities | Blocked PreloadAsync detection from " .. (s and s:GetFullName() or "Couldn't fetch") .. " | Method used: __namecall \n")
+            args[1] = tbl
+            return oldNamecall(self, unpack(args))
+        end
+    end
+
+    return oldNamecall(self, ...)
 end))
 
---// AntiKick: __index
-local isAdonis = false
+local function blockKickFunc(fn, label)
+    local orig
+    orig = hookfunction(fn, closure(function(...)
+        if not checkcaller() then
+            LMagentaPrint("szze's utilities | Blocked kick | Method used: __index " .. label .. "\n")
+            return wait(9e9)
+        end
+        return orig(...)
+    end))
+    return orig
+end
+
+local oldPreload
+oldPreload = hookfunction(ContentProvider.PreloadAsync, closure(function(self, ...)
+    local args = { ... }
+    if not checkcaller() and type(args[1]) == "table" and table.find(args[1], CoreGui) then
+        LMagentaPrint("szze's utilities | Blocked PreloadAsync detection | Method used: __index \n")
+        args[1] = tbl
+        return oldPreload(self, unpack(args))
+    end
+    return oldPreload(self, ...)
+end))
+
+blockKickFunc(game.Shutdown,  "[SHUTDOWN]")
+
 task.spawn(function()
-	repeat
-		task.wait()
-	until game:IsLoaded()
-	for i, v in next, game:GetService('ReplicatedStorage'):GetDescendants() do
-		if v.Name == "__FUNCTION" and v:IsA('RemoteFunction') and v.Parent:IsA('RemoteEvent') then
-			isAdonis = true
-		end
-	end
-	game:GetService('ReplicatedStorage').ChildAdded:Connect(function(v)
-		task.wait()
-		if v:IsA('RemoteEvent') and v:FindFirstChildWhichIsA('RemoteFunction') and v:FindFirstChildWhichIsA('RemoteFunction').Name == '__FUNCTION' then
-			isAdonis = true
-		end
-	end)
-	hookfunction(lplr.Destroy, closure(function(...)
-		local args = {
-			...
-		}
-		if not checkcaller() then
-			LMagentaPrint("szze's utilities | Blocked kick " .. '| Method used: __index [DESTROY] \n')
-			return wait(9e9)
-		end
-	end))
-	if isAdonis == false then
-		hookfunction(lplr.Kick, closure(function(...)
-			local args = {
-				...
-			}
-			if not checkcaller() and not isAdonis then
-				LMagentaPrint("szze's utilities | Blocked kick " .. '| Method used: __index \n')
-				return wait(9e9)
-			end
-		end))
-	else
-		rconsoleprint('@@RED@@')
-		rconsoleprint('Adonis detected! __index kick bypass will not work. \n')
-	end
+    repeat task.wait() until game:IsLoaded()
+
+    for _, v in next, ReplicatedStorage:GetDescendants() do
+        if v.Name == "__FUNCTION" and v:IsA("RemoteFunction") and v.Parent:IsA("RemoteEvent") then
+            isAdonis = true
+        end
+    end
+    ReplicatedStorage.ChildAdded:Connect(function(v)
+        task.wait()
+        if v:IsA("RemoteEvent") then
+            local f = v:FindFirstChildWhichIsA("RemoteFunction")
+            if f and f.Name == "__FUNCTION" then isAdonis = true end
+        end
+    end)
+
+    blockKickFunc(lplr.Destroy, "[DESTROY]")
+
+    if not isAdonis then
+        blockKickFunc(lplr.Kick, "")
+    else
+        rconsoleprint("@@RED@@")
+        rconsoleprint("Adonis detected! __index kick bypass will not work. \n")
+    end
 end)
-
---// AntiKick: Destroy() method
-
-local qwq
-qwq = hookmetamethod(game, '__namecall', closure(function(self, ...)
-	local kscriptz, kscript
-	local method = getnamecallmethod()
-	if self == lplr and string.lower(method) == 'destroy' and not checkcaller() then
-		kscriptz = getcallingscript()
-		if kscriptz then
-			kscript = kscriptz:GetFullName()
-		else
-			kscript = "Couldn't fetch"
-		end
-		LMagentaPrint("szze's utilities | Blocked kick from " .. tostring(kscript) .. ' | Method used: __namecall [DESTROY] \n')
-		return wait(9e9)
-	end
-	return qwq(self, ...)
-end))
-
---// Prints
-
-GreenPrint('Client Anti-Kick loaded \n')
-GreenPrint('VoiceChat spoofer loaded \n')
-GreenPrint('PreloadBypass loaded \n')
-GreenPrint('Memory bypasser loaded \n')
-GreenPrint('Local GamePass bypasser loaded \n')
-
---// AntiAFK
 
 local connections = getconnections or get_signal_cons
 if connections then
-	for i, v in next, connections(lplr.Idled) do
-		v:Disable()
-	end
+    for _, v in next, connections(lplr.Idled) do
+        v:Disable()
+    end
 end
-
-lplr.Idled:Connect(function(time)
-	if time > 1200 and not isAFK then
-		LMagentaPrint('AntiAFK bypassed the kick \n')
-		isAFK = true
-	end
+lplr.Idled:Connect(function(t)
+    if t > 1200 and not isAFK then
+        LMagentaPrint("AntiAFK bypassed the kick \n")
+        isAFK = true
+    end
 end)
 
-GreenPrint('AntiAFK loaded \n')
+GreenPrint("Client Anti-Kick loaded \n")
+GreenPrint("VoiceChat spoofer loaded \n")
+GreenPrint("PreloadBypass loaded \n")
+GreenPrint("Memory bypasser loaded \n")
+GreenPrint("Local GamePass bypasser loaded \n")
+GreenPrint("AntiAFK loaded \n")
 
---// Ver Checker
-
-local ver = 'V1.4'
-local verMain = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/CF-Trail/random/main/utilLoader/ver"))()
-
-if ver ~= verMain then
-	LMagentaPrint('Please update your utils script. -CF-Trail.')
+local ver = "V1.5"
+local ok, verMain = pcall(function()
+    return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/CF-Trail/random/main/utilLoader/ver"))()
+end)
+if ok and ver ~= verMain then
+    LMagentaPrint("Please update your utils script. -CF-Trail.")
 end
